0c88f200157d952717989dd34adbe9351dbc28c2
Arfan A. Khan <73901113+ArfanGits@users.noreply.github.com>
2022-01-02 15:19:21 +0600

    Admin constructor done

--- crud/src/Admin.php
+++ crud/src/Admin.php
@@ -5,20 +5,29 @@ namespace Bitm;
 use PDO;
 
 class Admin{
-    public function index(){
-        session_start();
 
+    public $id = null;
+    public $title = null;
+    public $conn = null;
+
+    public function __construct()
+    {
+        session_start();
         //Connect to database
-        $conn = new PDO("mysql:host=localhost;dbname=ecommerce",
+        $this->conn = new PDO("mysql:host=localhost;dbname=ecommerce",
             'root', '');
         //set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE,
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE,
             PDO::ERRMODE_EXCEPTION);
+    }
+
+
+    public function index(){
         
         
         $query = "SELECT * FROM `admin`";
         
-        $stmt = $conn->prepare($query);
+        $stmt = $this->conn->prepare($query);
         
         $result = $stmt->execute();
         
@@ -30,16 +39,10 @@ class Admin{
 
     public function show(){
         $_id = $_GET['id'];
-        //Connect to database
-        $conn = new PDO("mysql:host=localhost;dbname=ecommerce",
-            'root', '');
-        //set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE,
-            PDO::ERRMODE_EXCEPTION);
-
+        
         $query = "SELECT * FROM `admin` WHERE id = :id";
 
-        $stmt = $conn->prepare($query);
+        $stmt = $this->conn->prepare($query);
 
         $stmt->bindParam(':id', $_id);
 
@@ -51,7 +54,6 @@ class Admin{
     }
 
     public function store(){
-        session_start();
 
         $_name = $_POST['name'];
         $_email = $_POST['email'];
@@ -60,23 +62,10 @@ class Admin{
 
         $_created_at = date('Y-m-d h:i:s', time());
 
-        //echo $_title;
-
-        //Connect to database
-        $conn = new PDO(
-            "mysql:host=localhost;dbname=ecommerce",
-            'root',
-            ''
-        );
-        //set the PDO error mode to exception
-        $conn->setAttribute(
-            PDO::ATTR_ERRMODE,
-            PDO::ERRMODE_EXCEPTION
-        );
         $query = "INSERT INTO `admin` (`name`,`email`,`password`,`phone`,`created_at`) 
                 VALUES (:name, :email, :password,:phone,:created_at);";
 
-        $stmt = $conn->prepare($query);
+        $stmt = $this->conn->prepare($query);
 
         $result = $stmt->execute(array(
             ':name' => $_name,
@@ -104,20 +93,11 @@ class Admin{
     }
 
     public function delete(){
-        session_start();
-
         $_id = $_GET['id'];
-
-        //Connect to database
-        $conn = new PDO("mysql:host=localhost;dbname=ecommerce",
-            'root', '');
-        //set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE,
-            PDO::ERRMODE_EXCEPTION);
 
         $query = "DELETE FROM `admin` WHERE `admin`.`id` = :id";
 
-        $stmt = $conn->prepare($query);
+        $stmt = $this->conn->prepare($query);
 
         $stmt->bindParam(':id', $_id);
 
@@ -140,21 +120,9 @@ class Admin{
     public function edit(){
         $_id = $_GET['id'];
 
-        //Connect to database
-        $conn = new PDO(
-            "mysql:host=localhost;dbname=ecommerce",
-            'root',
-            ''
-        );
-        //set the PDO error mode to exception
-        $conn->setAttribute(
-            PDO::ATTR_ERRMODE,
-            PDO::ERRMODE_EXCEPTION
-        );
-
         $query = "SELECT * FROM `admin` WHERE id = :id";
 
-        $stmt = $conn->prepare($query);
+        $stmt = $this->conn->prepare($query);
 
         $stmt->bindParam(':id', $_id);
 
@@ -167,22 +135,13 @@ class Admin{
 
     public function update(){
 
-        session_start();
-
         $_id = $_POST['id'];
         $_name = $_POST['name'];
         $_email = $_POST['email'];
         $_phone = $_POST['phone'];
         $_password = $_POST['password'];
         $_modified_at = date('Y-m-d h:i:s',time());
-        //echo $_name;
-
-        //Connect to database
-        $conn = new PDO("mysql:host=localhost;dbname=ecommerce",
-            'root', '');
-        //set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE,
-            PDO::ERRMODE_EXCEPTION);
+       
         $query = "UPDATE `admin` SET `name` = :name, 
                                     `email` = :email, 
                                     `password` = :password,
@@ -190,7 +149,7 @@ class Admin{
                                     `modified_at` = :modified_at
                 WHERE `admin`.`id` = :id";
 
-        $stmt = $conn->prepare($query);
+        $stmt = $this->conn->prepare($query);
 
         $stmt->bindParam(':id', $_id);
         $stmt->bindParam(':name', $_name);
@@ -210,8 +169,6 @@ class Admin{
         header("location:index.php");
         return $result;
     }
-
-    
 
 
 }
