5868a569308893771b8473d12ad619ec35827ce5
Arfan A Khan <73901113+ArfanGits@users.noreply.github.com>
2022-01-04 17:32:49 +0600

    front updated

---
+++ crud/front/views/elements/carousel.php
@@ -0,0 +1,49 @@
+<?php
+
+use Bitm\Banner;
+
+$_banner = new Banner();
+
+$banners = $_banner->getActiveBanners();
+
+// var_dump($banners);
+
+?>
+
+<!--Markup for Carousel-->
+<div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
+
+    </div>
+    <div class="carousel-inner">
+        <?php
+        $_active = 'active';
+        foreach($banners as $banner):
+        ?>
+        <div class="carousel-item <?=$_active;?>">
+            <img src="<?=$webroot;?>uploads/<?=$banner['picture']?>" class="d-block w-100" alt="Slider Image">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+            </div>
+        </div>
+        <?php
+        $_active = '';
+        endforeach;
+        ?>
+
+
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
